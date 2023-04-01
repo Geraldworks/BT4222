6d74431fe202d65a556aa614541d50fadf00820c
szeying02 <limszeying02@gmail.com>
2023-04-02 00:21:59 +0800

    Added Documentation

--- resume_process.py
+++ resume_process.py
@@ -2,6 +2,11 @@ from bag_of_words import *
 from word_embedding import *
 
 def process_resume(input_file, dataset_difficulty):
+    '''the first output is the bag dataset, the second output is the embedding dataset. 
+    the left input is the file name, the right input is the "difficult" / "easy" 
+    (the number of columns differ for "difficult" and "easy" bags i believe
+    then the last line is just deciphering the predicted output'''
+    
     resume_file = open(input_file, "r")
     resume = resume_file.read()
     resume_file.close()
@@ -15,7 +20,7 @@ def process_resume(input_file, dataset_difficulty):
         X_bag = pd.DataFrame(get_tfidf(lemmatized_tokens, top_stems), columns=top_stems)
     
     # should output 100 columns
-    if dataset_difficulty == "difficult":
+    else:
         top_stems = [x[0] for x in pd.read_csv("top_stems.csv", header=None).values.tolist()]
         tokens = get_tokenized_text(resume)
         is_noun_or_adj = get_nouns_and_adjs(tokens)
